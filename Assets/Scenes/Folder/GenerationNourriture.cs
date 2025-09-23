ff5f238adfbcb362275c2da3adee72fb5c8c35f1
Baptiste Delavenne <baptistedelavenne80@gmail.com>
2025-09-23 19:35:44 +0200

    la pomme se déplace aléatoirement sur la carte quand on clique dessus

--- Assets/Scenes/Folder/GenerationNourriture.cs
+++ Assets/Scenes/Folder/GenerationNourriture.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GenerationNourriture : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float minX = -5f, maxX = 5f, minY = -5f, maxY = 5f;
+    public static int points = 1;
+
     void Start()
     {
-        
+        DeplacerPomme();
     }
 
-    // Update is called once per frame
-    void Update()
+    /* Fonction DeplacerPomme : 
+     * change les coordonées de la pomme
+     */
+    public void DeplacerPomme()
     {
-        
+        transform.position = new Vector3(
+            Random.Range(minX, maxX),
+            Random.Range(minY, maxY),
+            0
+        );
+    }
+
+    /* Fonction OnMouseDown : 
+     * appelée quand on clique sur la pomme
+     * appel la fonction DeplacerPomme
+     */
+    void OnMouseDown()
+    {
+        Debug.Log("Pomme cliquée !");
+        DeplacerPomme();
+    }
+
+
+    private void Collision(Collider2D other)
+    {
+        if (other.CompareTag("Pomme"))
+        {
+            GestionJeu.AjoutScore(points);
+            DeplacerPomme();
+        }
     }
 }
